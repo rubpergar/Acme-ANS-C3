0fd753c4de81e77d1e8c63e203381d4587915fad
macpercam15 <macpercam@gmail>
2025-03-26 13:16:41 +0100

    feature: controller

---
+++ src/main/java/acme/features/authenticated/assistanceAgent/claim/AssistanceAgentClaimController.java
@@ -0,0 +1,30 @@
+
+package acme.features.authenticated.assistanceAgent.claim;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.entities.claims.Claim;
+import acme.realms.AssistanceAgent;
+
+@GuiController
+public class AssistanceAgentClaimController extends AbstractGuiController<AssistanceAgent, Claim> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AssistanceAgentCompletedClaimListService listCompletedClaimsService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listCompletedClaimsService);
+
+	}
+
+}
