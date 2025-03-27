85ad214ee59cf3d18a23d26707cc113b4f228244
cyncasjua <cyncasjua@alum.us.es>
2025-03-27 22:26:00 +0100

    fix: Solo aircrafts activos cuando se crea un leg

--- src/main/java/acme/entities/aircrafts/AircraftRepository.java
+++ src/main/java/acme/entities/aircrafts/AircraftRepository.java
@@ -1,6 +1,8 @@
 
 package acme.entities.aircrafts;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -8,10 +10,10 @@ import acme.client.repositories.AbstractRepository;
 
 public interface AircraftRepository extends AbstractRepository {
 
-	@Query("select a from Aircraft a where a.registrationNumber = :registrationNumber")
-	Aircraft findByRegistrationNumber(String registrationNumber);
-
 	@Query("SELECT COUNT(a) > 0 FROM Aircraft a WHERE a.registrationNumber = :registrationNumber AND a.id <> :id")
 	boolean existsByRegistrationNumberAndNotId(@Param("registrationNumber") String registrationNumber, @Param("id") Integer id);
+
+	@Query("SELECT a FROM Aircraft a WHERE a.registrationNumber = :registrationNumber")
+	Optional<Aircraft> findAircraftByRegistrationNumber(String registrationNumber);
 
 }
--- src/main/java/acme/features/authenticated/manager/leg/ManagerLegRepository.java
+++ src/main/java/acme/features/authenticated/manager/leg/ManagerLegRepository.java
@@ -4,10 +4,12 @@ package acme.features.authenticated.manager.leg;
 import java.util.Collection;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
 import acme.entities.aircrafts.Aircraft;
+import acme.entities.aircrafts.AircraftStatus;
 import acme.entities.airports.Airport;
 import acme.entities.flights.Flight;
 import acme.entities.legs.Leg;
@@ -27,8 +29,8 @@ public interface ManagerLegRepository extends AbstractRepository {
 	@Query("select a from Airport a")
 	Collection<Airport> findAllAirports();
 
-	@Query("select a from Aircraft a")
-	Collection<Aircraft> findAllAircrafts();
+	@Query("select a from Aircraft a where a.status = :status")
+	Collection<Aircraft> findAllAircraftsByStatus(@Param("status") AircraftStatus status);
 
 	@Query("select a from Aircraft a where a.id = :aircraftId")
 	Aircraft findAircraftById(int aircraftId);
