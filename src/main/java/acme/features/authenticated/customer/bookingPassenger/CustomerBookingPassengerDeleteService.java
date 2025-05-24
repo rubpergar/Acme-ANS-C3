a42cf85c9766f8f169a50ed24135e008bd9bbf57
Laura Pérez Franco <lauperfra@alum.us.es>
2025-05-24 14:28:25 +0200

    Arreglo Delete BookingPassenger

--- src/main/java/acme/features/authenticated/customer/bookingPassenger/CustomerBookingPassengerDeleteService.java
+++ src/main/java/acme/features/authenticated/customer/bookingPassenger/CustomerBookingPassengerDeleteService.java
@@ -1,12 +1,18 @@
 
 package acme.features.authenticated.customer.bookingPassenger;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.booking.BookingPassenger;
+import acme.entities.passenger.Passenger;
 import acme.realms.Customer;
 
 @GuiService
@@ -22,19 +28,14 @@ public class CustomerBookingPassengerDeleteService extends AbstractGuiService<Cu
 	@Override
 	public void authorise() {
 
-		BookingPassenger bookingPassenger;
-		int bookingId;
-		int userAccountId;
-		userAccountId = super.getRequest().getPrincipal().getAccountId();
-		bookingId = super.getRequest().getData("id", int.class);
-		bookingPassenger = this.repository.findBookingPassengerById(bookingId);
+		Passenger passenger;
+		int id = super.getRequest().getData("id", int.class);
+		passenger = this.repository.findPassengerByBookingPassengerId(id);
+		int userAccountId = super.getRequest().getPrincipal().getAccountId();
+		BookingPassenger bookingPassenger = this.repository.findBookingPassengerById(id);
 
-		boolean status = bookingPassenger.getBooking().getCustomer().getUserAccount().getId() == userAccountId;
-
-		if (!bookingPassenger.getBooking().getIsDraft())
-			status = false;
-
-		super.getResponse().setAuthorised(status);
+		boolean bookingIsDraft = bookingPassenger.getBooking().getIsDraft() == true;
+		super.getResponse().setAuthorised(passenger.getCustomer().getUserAccount().getId() == userAccountId && bookingIsDraft);
 	}
 
 	@Override
@@ -56,13 +57,32 @@ public class CustomerBookingPassengerDeleteService extends AbstractGuiService<Cu
 
 	@Override
 	public void perform(final BookingPassenger bookingPassenger) {
-		this.repository.delete(bookingPassenger);
+		BookingPassenger bookingPassengerToDelete = this.repository.findBookingPassengerByBookingIdAndPassengerId(bookingPassenger.getBooking().getId(), bookingPassenger.getPassenger().getId());
+		this.repository.delete(bookingPassengerToDelete);
 	}
 
 	@Override
 	public void unbind(final BookingPassenger bookingPassenger) {
 		Dataset dataset;
-		dataset = super.unbindObject(bookingPassenger, "booking", "passenger");
+		SelectChoices passengers = null;
+		int customerId;
+		customerId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		Collection<Passenger> publishedPassengers = this.repository.findAllPublishedPassengersByCustomerId(customerId);
+		Collection<Passenger> passengerAlreadyInBooking = this.repository.findPassengersByBookingId(bookingPassenger.getBooking().getId());
+
+		List<Passenger> passengersNotInBooking = publishedPassengers.stream().filter(p -> !passengerAlreadyInBooking.contains(p)).toList();
+
+		dataset = super.unbindObject(bookingPassenger, "booking", "passenger", "id");
+		dataset.put("bookingLocatorCode", bookingPassenger.getBooking().getLocatorCode());
+		dataset.put("bookingId", bookingPassenger.getBooking().getId());
+
+		List<Passenger> listPassengersNotInBooking = new ArrayList<>(passengersNotInBooking);
+		//if (!passengersNotInBooking.isEmpty()) {
+		if (!passengersNotInBooking.contains(bookingPassenger.getPassenger()))
+			listPassengersNotInBooking.add(bookingPassenger.getPassenger());
+		passengers = SelectChoices.from(listPassengersNotInBooking, "fullName", bookingPassenger.getPassenger());
+		dataset.put("passenger", passengers);
+
 		super.getResponse().addData(dataset);
 	}
 
