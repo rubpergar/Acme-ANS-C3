b6c9b007a5b6902ce6063d0eed6a8093859946cc
Laura Pérez Franco <lauperfra@alum.us.es>
2025-05-25 17:47:33 +0200

    testSuiteSt2

--- src/main/java/acme/entities/passenger/Passenger.java
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -4,9 +4,7 @@ package acme.entities.passenger;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -25,9 +23,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(indexes = {
-	@Index(columnList = "customer_id"), @Index(columnList = "isDraft"), @Index(columnList = "customer_id, isDraft")
-})
+//@Table(indexes = {
+//	@Index(columnList = "customer_id"), @Index(columnList = "isDraft"), @Index(columnList = "customer_id, isDraft")
+//})
 public class Passenger extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
