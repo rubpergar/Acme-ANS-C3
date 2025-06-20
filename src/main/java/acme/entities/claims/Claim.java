534b29ef0a3942e24b5413eed82dd6398f6311c8
macpercam15 <macpercam@gmail>
2025-06-20 14:10:26 +0200

    fix: requisito 11, indices ("Índices redundantes.")

--- src/main/java/acme/entities/claims/Claim.java
+++ src/main/java/acme/entities/claims/Claim.java
@@ -6,9 +6,7 @@ import java.util.Collection;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -32,9 +30,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidClaim
-@Table(indexes = {
-	@Index(columnList = "assistance_agent_id")
-})
+//@Table(indexes = {
+//	@Index(columnList = "assistance_agent_id")
+//})
 public class Claim extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
