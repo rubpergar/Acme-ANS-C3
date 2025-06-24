c032c88389918573b3306aedae485cadf1403706
Laura Pérez Franco <lauperfra@alum.us.es>
2025-06-24 16:53:42 +0200

    fix: Indices Student 2

--- src/main/java/acme/entities/booking/Booking.java
+++ src/main/java/acme/entities/booking/Booking.java
@@ -6,9 +6,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
@@ -32,9 +30,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(indexes = {
-	@Index(columnList = "locatorCode"), @Index(columnList = "customer_id")
-})
 public class Booking extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
--- src/main/java/acme/realms/Customer.java
+++ src/main/java/acme/realms/Customer.java
@@ -3,8 +3,8 @@ package acme.realms;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Index;
-import javax.persistence.Table;
+// import javax.persistence.Index;
+// import javax.persistence.Table;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
@@ -20,9 +20,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(indexes = {
-	@Index(columnList = "user_account_id")
-})
 public class Customer extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
