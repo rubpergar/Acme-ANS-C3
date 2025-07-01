8cc0d53f160b30e515a8e571d3c431783f38afaa
cyncasjua <cyncasjua@alum.us.es>
2025-07-01 21:40:37 +0200

    mock para weather y pruebas

--- src/main/java/acme/features/any/weatherDashboard/AnyWeatherDashboardListService.java
+++ src/main/java/acme/features/any/weatherDashboard/AnyWeatherDashboardListService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.models.Dataset;
 import acme.client.components.principals.Any;
+import acme.client.helpers.MomentHelper;
+import acme.client.helpers.SpringHelper;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.weathers.Weather;
@@ -34,7 +36,12 @@ public class AnyWeatherDashboardListService extends AbstractGuiService<Any, Weat
 
 		List<WeatherDashboard> weatherDashboards = new ArrayList<>();
 
-		List<Weather> weathers = this.repository.findAllWeathers();
+		List<Weather> weathers;
+
+		if (SpringHelper.isRunningOn("production"))
+			weathers = this.repository.findAllWeathers();
+		else
+			weathers = this.getMockedWeathers();
 
 		for (Weather w : weathers) {
 			WeatherDashboard wD = new WeatherDashboard();
@@ -48,7 +55,6 @@ public class AnyWeatherDashboardListService extends AbstractGuiService<Any, Weat
 			wD.setHumidity(w.getHumidity());
 			weatherDashboards.add(wD);
 		}
-
 		super.getBuffer().addData(weatherDashboards);
 	}
 
@@ -61,6 +67,35 @@ public class AnyWeatherDashboardListService extends AbstractGuiService<Any, Weat
 		dataset.put("humidity", weathersDashboard.getHumidity());
 		super.addPayload(dataset, weathersDashboard, "description", "temperature", "windSpeed", "humidity");
 		super.getResponse().addData(dataset);
+	}
+
+	private List<Weather> getMockedWeathers() {
+		List<Weather> mocked = new ArrayList<>();
+
+		Weather w1 = new Weather();
+		w1.setId(1);
+		w1.setCity("Sevilla");
+		w1.setCountry("España");
+		w1.setDate(MomentHelper.parse("2024-12-04 10:00", "yyyy-MM-dd HH:mm"));
+		w1.setDescription("heavy rain");
+		w1.setTemperature(18.0);
+		w1.setWindSpeed(35.0);
+		w1.setHumidity(90.0);
+
+		Weather w2 = new Weather();
+		w2.setId(2);
+		w2.setCity("Madrid");
+		w2.setCountry("España");
+		w2.setDate(MomentHelper.parse("2024-12-05 10:00", "yyyy-MM-dd HH:mm"));
+		w2.setDescription("snow");
+		w2.setTemperature(30.0);
+		w2.setWindSpeed(10.0);
+		w2.setHumidity(40.0);
+
+		mocked.add(w1);
+		mocked.add(w2);
+
+		return mocked;
 	}
 
 }
