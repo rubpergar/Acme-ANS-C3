ce15059be832822ce84231cd3ee6a27d664b17ee
macpercam15 <macpercam@gmail>
2025-07-03 19:21:57 +0200

    fix: indices corregidos

--- src/main/java/acme/entities/trackingLogs/TrackingLog.java
+++ src/main/java/acme/entities/trackingLogs/TrackingLog.java
@@ -4,7 +4,9 @@ package acme.entities.trackingLogs;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -25,9 +27,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidTrackingLog
-//@Table(indexes = {
-//	@Index(columnList = "claim_id")
-//})
+@Table(indexes = {
+	@Index(columnList = "claim_id, lastUpdate")
+})
 public class TrackingLog extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
